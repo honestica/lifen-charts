b1cadf5c2cd3cdf0c0ea1ae8a65bbdfb9ac38067
--replace-all <matthieu.paret@lifen.fr>
2019-09-01 16:15:14 +0200

    Add AWX and E2E tests

---
+++ scripts/setup_cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set +x
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Do not switch kubectl context before the end of this script!"
+
+minikube addons enable metrics-server
+
+# Ensure we run on minikube
+kubectl config use-context minikube
+
+# Create the namespace where this application will be deployed:
+
+kubectl apply -f $DIR/cluster_setup.yaml 
+
+# Install helm 
+
+helm init --wait --upgrade
+
+helm upgrade nginx-ingress-internal --set controller.ingressClass=nginx --set controller.service.enableHttps=false stable/nginx-ingress --install --kube-context minikube --namespace ingress

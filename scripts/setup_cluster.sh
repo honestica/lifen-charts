106b0d9774426271bdc5d7468d40955638ca0d46
--replace-all <matthieu.paret@lifen.fr>
2019-09-02 09:58:27 +0200

    fix few things

--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -12,6 +12,7 @@ minikube addons enable metrics-server
 # Ensure we run on minikube
 kubectl config use-context minikube
 
+# Install calico
 curl https://docs.projectcalico.org/v3.8/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
@@ -22,7 +23,5 @@ kubectl apply -f $DIR/cluster_setup.yaml
 # Install helm 
 
 helm init --wait --upgrade
-
-# Install calico
 
 helm upgrade nginx-ingress-internal --set controller.ingressClass=nginx --set controller.service.enableHttps=false stable/nginx-ingress --install --kube-context minikube --namespace ingress
